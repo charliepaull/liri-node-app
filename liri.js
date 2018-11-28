a93a3d3f57be998d36e41d1977f4c4291051349d
Charlie Paull <charliepaull211@gmail.com>
2018-11-28 14:16:58 -0800

    axios calls for bandsintown & omdb are responsive, and linked to argv.2 & argv.3

--- liri.js
+++ liri.js
@@ -11,37 +11,37 @@ var axios = require("axios");
 // begin Axios
 // Axios for OMDB
 // user enters movie title
-// var movieName = "";
-// // queryURL has OMDB api key
-// var queryURL1 = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// // console.log(queryURL1)
-// // create axios object get request
-// axios.get(queryURL1).then(
-//     // callback function to get response
-//     function(response) {
-//         console.log(response)
-//     }
-// );
+var movieName = userChoice;
+// queryURL has OMDB api key
+var queryURL1 = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// console.log(queryURL1)
+// create axios object get request
+axios.get(queryURL1).then(
+    // callback function to get response
+    function(response) {
+        console.log(response.data)
+    }
+);
 
 // BandsInTown Axios
-var artist = "";
+var artist = userChoice;
 // bandsintown queryURL/api
 var QueryURL2 = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 console.log("queryURL2")
 axios.get(QueryURL2).then(
     function(response){
-        console.log(response.body)
+        console.log(response.data)
     }
 )
 
-// create command line interfaces for the user
+// // create command line interfaces for the user
 var userInput1 = process.argv[2];
-// var userChoice = process.argv[3];
-// logic of 4 possible commands:
-    // concert-this
-    // spotify-this-song
-    // movie-this
-    // do-what-it-says
+var userChoice = process.argv[3];
+// // logic of 4 possible commands:
+//     // concert-this
+//     // spotify-this-song
+//     // movie-this
+//     // do-what-it-says
 // use switch-case 
 switch (userInput1){
     case "concert-this":
@@ -49,23 +49,23 @@ switch (userInput1){
     console.log("Find a concert")
     break;
 
-    case "spotify-this-song":
-    spotifySong();
-    console.log("What song is this?")
-    break;
+//     case "spotify-this-song":
+//     spotifySong();
+//     console.log("What song is this?")
+//     break;
 
-    case "movie-this":
-    console.log("Movie stats")
-    // movieInfo();
-    break;
+//     case "movie-this":
+//     console.log("Movie stats")
+//     // movieInfo();
+//     break;
 
-    case "do-what-it-says":
-    default: console.log("Nothing there");
-}
+//     case "do-what-it-says":
+//     default: console.log("Nothing there");
+// }
 
 // var details = {};
 // function findConcert(){
 //     switch (userChoice){
 //         case details:
 //     }
-// }
+};
