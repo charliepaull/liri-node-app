96662853693a2a71f0f09e22e23b5c7f05cd1518
Charlie Paull <charliepaull211@gmail.com>
2018-11-28 16:27:49 -0800

    last push at 4:30pm 11/28. Figuring out Spotify object, logic to print out object still to come.

--- liri.js
+++ liri.js
@@ -1,9 +1,27 @@
 // push into liri.js the .env folder with
 // only my local ID & SECRET keys
 require("dotenv").config();
+// push in key.js
+var keys = require("./key.js");
+// require npm Spotify package
+var Spotify = require("node-spotify-api");
 // push in keys.js, which has .env codes linked to it.
 // this links in API for Spotify
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+// use to format & join into a string from each user input after process.argv[3]
+var spotifyDetails = process.argv.slice(3).join(" ");
+// start Spotify request
+function spotifySong(userChoice){
+spotify.search({type:"track", query: userChoice, limit: 3}, function(err, data){
+    if (err){
+        return console.log("Error occured + err");
+    }
+    
+    console.log(JSON.stringify(data[0], null, 2));
+    // var spotifyDetails = data[0];
+});
+}
+
 // used to trigger and require Axios for this file
 // module already installed
 var axios = require("axios");
@@ -24,15 +42,17 @@ axios.get(queryURL1).then(
 );
 
 // BandsInTown Axios
+function findConcert(){
 var artist = userChoice;
 // bandsintown queryURL/api
 var QueryURL2 = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-console.log("queryURL2")
+// console.log(QueryURL2)
 axios.get(QueryURL2).then(
     function(response){
         console.log(response.data)
     }
 )
+}
 
 // // create command line interfaces for the user
 var userInput1 = process.argv[2];
@@ -45,14 +65,14 @@ var userChoice = process.argv[3];
 // use switch-case 
 switch (userInput1){
     case "concert-this":
-    // findConcert();
-    console.log("Find a concert")
+    findConcert();
+    console.log("Find a concert");
     break;
 
-//     case "spotify-this-song":
-//     spotifySong();
-//     console.log("What song is this?")
-//     break;
+    case "spotify-this-song":
+    spotifySong();
+    console.log("What song is this?");
+    break;
 
 //     case "movie-this":
 //     console.log("Movie stats")
