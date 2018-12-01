7eb8fef6e0cc0f2a3f1633b0c45a634707f1d87d
Charlie Paull <charliepaull211@gmail.com>
2018-11-30 16:35:37 -0800

    all info from object prints, need to reformat findConcert() function.

--- liri.js
+++ liri.js
@@ -19,11 +19,20 @@ var userSearch = process.argv.slice(3).join(" ");
 
 function spotifySong(userSearch){
 // start Spotify request
-spotify.search({type:"track", query: userSearch, limit: 3}, function(err, data){
+spotify.search({type:"track", query: userSearch, limit: 1}, function(err, data){
     if (err){
         return console.log("Error occured + err");
     }
-    console.log(data.tracks.items[0].album);
+    // console.log(data.tracks.items[0]);
+    // Here, add these points from the response object:
+        // artist(s)
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        // song name
+        console.log("Song Name: " + data.tracks.items[0].name);
+        // preview link of the song from Spotify
+        console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+        // album name
+        console.log("Album Name: " + data.tracks.items[0].album.name);
     // console.log(JSON.stringify(data[0]));
 });
 };
@@ -38,7 +47,24 @@ var queryURL1 = "http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apik
 axios.get(queryURL1).then(
     // callback function to get response
     function(response) {
-        console.log(response.data)
+        // console.log(response.data)
+    // Here, add these points from the response object
+        // title of the movie
+        console.log("Title: " + response.data.Title);
+        // year movie was released
+        console.log("Year Released: " + response.data.Year);
+        // imdb rating
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        // rotten tomatoes rating
+        console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value)
+        // country where movie was produced
+        console.log("Country: " + response.data.Country)
+        // language of the movie
+        console.log("Language: " + response.data.Language);
+        // plot 
+        console.log("Plot: " + response.data.Plot);
+        // actors
+        console.log("Actors: " + response.data.Actors);
     }
 );
 };
@@ -49,7 +75,23 @@ function findConcert(userSearch){
     var QueryURL2 = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
     axios.get(QueryURL2).then(
         function(response){
-            console.log(response.data)
+            // console.log(response)
+            // console.log(response.data[0])
+        // Here, add these points from the response object:
+            // Artist name
+            console.log("Artist: " + response.data[0].lineup[0])
+            // Venue Name
+            console.log("Venue: " + response.data[0].venue.name)
+            // Venue location
+            // var locationArr = [];
+            var city = console.log(response.data[0].venue.city);
+            var state = console.log(response.data[0].venue.region);
+            // locationArr.push(city, state);
+            // console.log(locationArr);
+            // Event Date (using mm/dd/yyyy)
+            var date = console.log(response.data[0].datetime);
+            // var d = new Date("2019-05-03T12:00:00"); 
+            // console.log(d.toLocaleString()); 
         }
     );
 
