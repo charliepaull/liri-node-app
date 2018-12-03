2995a815f263ed4a46591523d751b84f77b21ef9
Charlie Paull <charliepaull211@gmail.com>
2018-12-02 17:11:42 -0800

    add moment for concert date. fixed functionality issues with using liri3.js. transfer back to liri.js after completion

--- liri3.js
+++ liri3.js
@@ -17,44 +17,19 @@ var command = process.argv[2];
 // use to format & join into a string from each user input after process.argv[2]
 var userSearch = process.argv.slice(3).join(" ");
 
-var fs = require("fs")
+var moment = require("moment");
 
 function spotifySong(userSearch){
-    console.log('spotify is working');
 // start Spotify request
-    spotify.search({type:"track", query: userSearch, limit: 1}, function(err, data){
-    // if (err){
-    //     // Styx
-    //     console.log("Artist: Styx");
-    //     // Too Much Time on My Hands
-    //     console.log("Song Name: Too Much Time on My Hands");
-    //     // preview link of the song from Spotify
-    //     console.log("Link: https://open.spotify.com/album/6PhLTeuN0G894bdSBTCwUF");
-    //     // album name
-    //     console.log("Album Name: Paradise Theatre");
-    // }
-
-    if (command === "do-what-it-says"){
-        fs.readFile("random.txt", "utf8", function(error, data) {
-            // If the code experiences any errors it will log the error to the console.
-            if (error) {
-            return console.log(error);
-            }
-
-            // have to concert this from a string in the .txt file to process.argv
-
-            // We will then print the contents of data
-            console.log("Artist: " + data.tracks.items[0].artists[0].name);
-            // song name
-            console.log("Song Name: " + data.tracks.items[0].name);
-            // preview link of the song from Spotify
-            console.log("Link: " + data.tracks.items[0].external_urls.spotify);
-            // album name
-            console.log("Album Name: " + data.tracks.items[0].album.name);
-
-        });
-    
-        // artist(s2)
+    if (!userSearch){
+        userSearch = "Too Much Time on My Hands"
+    }
+spotify.search({type:"track", query: userSearch, limit: 1}, function(err, data){
+    if (err){
+        return console.log("Error occured + err");
+    }
+    // Here, add these points from the response object:
+        // artist(s)
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
         // song name
         console.log("Song Name: " + data.tracks.items[0].name);
@@ -62,43 +37,24 @@ function spotifySong(userSearch){
         console.log("Link: " + data.tracks.items[0].external_urls.spotify);
         // album name
         console.log("Album Name: " + data.tracks.items[0].album.name);
-        
-    };
-    
+        console.log("")
 });
-
+};
 
 // begin Axios
 // Axios for OMDB
 // queryURL has OMDB api key
+
 function findMovie(userSearch){
-    console.log(userSearch);
+    if (!userSearch){
+        userSearch = "Mr. Nobody"
+    }
 var queryURL1 = "http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy";
-console.log(queryURL1)
+// console.log(queryURL1)
 // create axios object get request
 axios.get(queryURL1).then(
     // callback function to get response
     function(response) {
-        if (userSearch === ""){
-            // Movie
-            console.log("Title: Mr. Nobody");
-            // Too Much Time on My Hands
-            console.log("Year Released: 2009");
-            // preview link of the song from Spotify
-            console.log("IMDB Rating: 7.9");
-            // album name
-            console.log("Rotten Tomatoes rating: 67%");
-            // country where movie was produced
-            console.log("Country: Belgium, Germany, Canada, France, USA, UK")
-        // language of the movie
-            console.log("Language: English, Mohawk");
-            // plot 
-            console.log("Plot: A boy stands on a station platform as a train is about to leave.\nShould he go with his mother or stay with his father? Infinite possibilities arise from this decision.\nAs long as he doesn't choose, anything is possible.");
-            // actors
-            console.log("Actors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham");
-            }
-
-        else{
         // console.log(response.data)
     // Here, add these points from the response object
         // title of the movie
@@ -118,13 +74,15 @@ axios.get(queryURL1).then(
         // actors
         console.log("Actors: " + response.data.Actors);
     }
-    }
 );
 };
 
 // BandsInTown Axios
 function findConcert(userSearch){
-    console.log("find concert");
+    if (!userSearch){
+        console.log("Go find yourself a Nickleback concert, you dweeb.");
+        return
+    }
     // bandsintown queryURL/api
     var QueryURL2 = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
     axios.get(QueryURL2).then(
@@ -140,24 +98,17 @@ function findConcert(userSearch){
             // var locationArr = [];
             var city = console.log(response.data[0].venue.city);
             var state = console.log(response.data[0].venue.region);
-            var country = console.log(response.data[0].venue.country);
             // locationArr.push(city, state);
             // console.log(locationArr);
             // Event Date (using mm/dd/yyyy)
-            // var date = (moment(new Date()).format("DD/MM/YYYY").response.data[0].datetime);
-            // console.log(date);
-            // var d = new Date("2019-05-03T12:00:00"); 
-            // console.log(d.toLocaleString()); 
+            var date = response.data[0].datetime;
+            date = moment(new Date()).format("DD/MM/YYYY");
+            console.log(date);
         }
     );
 
 };
 
-
-if (command === 'movie-this') {
-    console.log('find movie test');
-    findMovie(userSearch);
-}
 // // logic of 4 possible commands:
 //     // concert-this
 //     // spotify-this-song
@@ -167,24 +118,23 @@ if (command === 'movie-this') {
 switch (command){
     case "concert-this":
     findConcert(userSearch);
-    console.log("Concert Info: ");
+    console.log("Concert ");
     break;
 
     case "spotify-this-song":
     spotifySong(userSearch);
-    console.log("Song Info: ");
+    console.log("What song is this?");
     break;
 
     case "movie-this":
     findMovie(userSearch);
-    console.log("Movie Info: ")
+    console.log("Movie stats")
+    // movieInfo();
     break;
 
     case "do-what-it-says":
-    spotifySong();
-    console.log("Read file");
+    console.log("Nothing there");
     break;
 
     default: console.log("Nothing entered");
-}};
-
+};
